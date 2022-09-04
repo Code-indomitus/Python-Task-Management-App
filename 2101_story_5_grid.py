c536087f5b66ecd282b43658056c7e13daa06d35
lcar0029 <lcar0029@student.monash.edu>
2022-09-04 17:40:39 +0800

    Added more frames to test if program can support additional grids; Now can accomodate a 3x3 grid size; Implemented loops to reduce code repition for frame/card positioning; Reduced use of literals so the grid size can be determined dynamically

--- 2101_story_5_grid.py
+++ 2101_story_5_grid.py
@@ -1,29 +1,73 @@
 import tkinter as tk
-from tkinter import *
+import math
+from tkinter import *\
+    
+# TODO: add a new page if first page cannot display all frames
+    
+# functions
+# application
+def main():
+    
+    # attributes
+    frameArray = []
 
-# root 
-root = tk.Tk()
+    # root 
+    root = tk.Tk()
 
-# master window
-root.title("Sprint Master")
-root.geometry("1000x500")
+    # master window
+    root.title("Sprint Master")
+    root.geometry("1000x500")
 
-# CREATE FRAME
-# w = frame( master, options)
-frame1 = tk.Frame(root, bg = "cyan", width=200, height=200)
-frame2 = tk.Frame(root, bg = "magenta", width=200, height=200)
-frame3 = tk.Frame(root, bg = "yellow", width = 200, height=200)
+    # CREATE FRAME
+    # w = frame( master, options)
+    masterFrame = Frame(root, bg = "gray", width=1000, height=500)
+    frame1 = Frame(masterFrame, bg = "cyan", width=100, height=100)
+    frame2 = Frame(masterFrame, bg = "magenta", width=100, height=100)
+    frame3 = Frame(masterFrame, bg = "yellow", width=100, height=100)
+    frame4 = Frame(masterFrame, bg = "red", width=100, height=100)
+    frame5 = Frame(masterFrame, bg = "green", width=100, height=100)
+    frame6 = Frame(masterFrame, bg = "blue", width=100, height=100)
+    frame7 = Frame(masterFrame, bg = "pink", width=100, height=100)
+    frame8 = Frame(masterFrame, bg = "white", width=100, height=100)
+    frame9 = Frame(masterFrame, bg = "black", width=100, height=100)
 
-root.grid_rowconfigure(6, weight=1)
-root.grid_columnconfigure(0, weight=1)
+    # ADD FRAME
+    # TODO: replace with object's .add() method
+    addFrame(frameArray, frame1)
+    addFrame(frameArray, frame2)
+    addFrame(frameArray, frame3)
+    addFrame(frameArray, frame4)
+    addFrame(frameArray, frame5)
+    addFrame(frameArray, frame6)
+    addFrame(frameArray, frame7)
+    addFrame(frameArray, frame8)
+    addFrame(frameArray, frame9)
+    
+    # determine amount of rows and cols required to display all cards
+    requiredRows = math.ceil(math.sqrt(len(frameArray)))
+    requiredCols = math.ceil(math.sqrt(len(frameArray)))
 
-frame1.grid(row = 0, rowspan = 1, pady = 5)
-frame2.grid(row = 2, rowspan = 1, pady = 5)
-frame3.grid(row = 4, rowspan = 1, stick = "n", pady = 5)
+    # set grid
+    root.grid_rowconfigure(requiredRows, weight=1)
+    root.grid_columnconfigure(requiredCols, weight=1)
+    
+    # set master frame's position
+    masterFrame.grid(row = 2, column = 2, rowspan = requiredRows, columnspan = requiredCols, padx = 10, pady = 10)
+    
+    # set each child frame in master frame
+    currRowspan = 1
+    currPadY = 5
+    currPadX = 5
+    for col in range(0, requiredCols):
+        for row in range(0, requiredRows):
+            frameArray[row + col*3].grid(row = row, column = col, rowspan = currRowspan, 
+                                         padx = currPadX, pady = currPadY)
 
-# label to add gap at top
-#topGap = Label(root, text="hi")
-#topGap.place(relx=0.5, anchor = 'n')
+    # run   
+    root.mainloop()
 
-# run   
-root.mainloop()
+# add frame to array (TODO: replaced by card object later)
+def addFrame(frameArray, thisFrame):
+    frameArray.append(thisFrame)
+    
+main()
