2d52d8584f982f6b53b6daa8b305a00982fb05a3
sbor0018 <sbor0018@student.monash.edu>
2022-09-30 18:45:03 +0800

    Added the sprint tab to the gui application.

--- shyam_spikes_Proj_Iter_2/tabs.py
+++ shyam_spikes_Proj_Iter_2/tabs.py
@@ -1,6 +1,7 @@
 import sqlite3
 from tkinter import *
 from tkinter import ttk
+from tkcalendar import DateEntry
 
 main_window = Tk()
 main_window.title("SprintMaster")
@@ -26,7 +27,10 @@ def createNewTaskWindow():
         connect_db = sqlite3.connect('tasks.db')
         # Create cusror
         cursor = connect_db.cursor()
-
+        cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS tasks
+                    ([task_name], [task_description], [story_points], [priority], [status], [assigned_to], [tag], [id])
+                    ''')
         connect_db.execute("INSERT INTO tasks VALUES (:task_name, :task_description, :stroy_points, :priority, :status, :assigned_to, :tag)", 
                         {
                             'task_name': entry1.get(),
@@ -132,10 +136,82 @@ def createNewTaskWindow():
     discardButton = Button(frame, text = "Close", command = newTaskWindow.destroy)
     discardButton.place(x = 225, y = 350)
 
+def createNewSprintWindow():
+
+    def createSprint():
+        # Create/Connect to a database
+        connect_db = sqlite3.connect('sprints.db')
+        # Create cusror
+        cursor = connect_db.cursor()
+
+        cursor.execute('''
+                CREATE TABLE IF NOT EXISTS sprints
+                ([sprint_name], [start_date], [end_date])
+                ''')
+
+        connect_db.execute("INSERT INTO sprints VALUES (:sprint_name, :start_date, :end_date)", 
+                        {
+                            'sprint_name': sprint_name_entry.get(),
+                            'start_date': start_date_entry.get_date().strftime("%m/%d/%Y"),
+                            'end_date': end_date_entry.get_date().strftime("%m/%d/%Y")
+                        }
+                       
+                            )
+        # Commit changes
+        connect_db.commit()
+        # Close Connnection
+        connect_db.close()
+
+        # Clear input boxes
+        sprint_name_entry.delete(0, END)
+        start_date_entry.delete(0, END)
+        end_date_entry.delete(0, END)
+
+     
+    # Toplevel object which will
+    # be treated as a new window
+    newSprintWindow = Toplevel(main_window)
+ 
+    # sets the title of the
+    # Toplevel widget
+    newSprintWindow.title("New Sprint")
+ 
+    # sets the geometry of toplevel
+    newSprintWindow.geometry("400x400")
+
+
+    frame = Frame(newSprintWindow, width = 400, height = 400)
+    frame.pack()
+
+    sprint_name = Label(frame, text = "Sprint Name:")
+    sprint_name.place(x = 20, y = 50)
+
+    start_date = Label(frame, text = "Start Date:")
+    start_date.place(x = 20, y = 90)
+
+    end_date = Label(frame, text = "End Date:")
+    end_date.place(x = 20, y = 140)
+
+    sprint_name_entry = Entry(frame, width = 40)
+    start_date_entry = DateEntry(frame,selectmode='day')  # Date entry allows user to get date input using a calendar. The date input is in datetime.date format/object
+    end_date_entry = DateEntry(frame,selectmode='day')
+    sprint_name_entry.place(x = 110, y = 50)
+    start_date_entry.place(x = 110, y = 90)
+    end_date_entry.place(x = 110, y = 140)
+
+
+
+    createButton = Button(frame, text = "Create Sprint", command = createSprint)
+    createButton.place(x = 110, y = 290)
+
+    discardButton = Button(frame, text = "Discard Sprint", command = newSprintWindow.destroy)
+    discardButton.place(x = 225, y = 290)
 
 createTaskButton = Button(tab1, text = "Create New Task", command = createNewTaskWindow)
 createTaskButton.place(x = 20, y = 20)
 
+createSprintButton = Button(tab2, text = "Create New Sprint", command = createNewSprintWindow)
+createSprintButton.place(x = 20, y = 20)
 
 notebook.add(tab1, text = "Task Board")
 notebook.add(tab2, text = "Sprint Board")
